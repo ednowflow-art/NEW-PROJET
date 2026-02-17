e4cb38e60ead09bbb03ee943e787e0fba533e030
ednowflow-art <ednowflow@gmail.com>
2026-02-17 16:59:03 +0100

    nbrpaire

--- moyenne.c
+++ moyenne.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-int main(){
+    #include <stdio.h>
+    int main(){
 
-int x,n,somme,cpt;
- float moyenne;
- do{
-    printf("saisir n entiers:");
-    scanf("%d",&n);
- }while(n<0);
- for(int i=0; i<n; i++){
-    do{
-        puts("veuillez saisir un entier negatif:");
-        scanf("%d",&x);
-    }while(x>0);
-     if(x%2==0){
-        somme= somme+x;
-        cpt++;
+    int x,n,somme,cpt;
+     float moyenne;
+     do{
+        printf("saisir n entiers:");
+        scanf("%d",&n);
+     }while(n<0);
+     for(int i=0; i<n; i++){
+        do{
+            puts("veuillez saisir un entier negatif:");
+            scanf("%d",&x);
+        }while(x>0);
+         if(x%2==0){
+            somme= somme+x;
+            cpt++;
+         }
      }
- }
-  moyenne= somme/cpt;
-  printf("la moyenne est : %f",moyenne)
- return 0;
+      moyenne= somme/cpt;
+      printf("la moyenne des nombres pair est : %f",moyenne);
 
 
-    return 0;
-}
+
+        return 0;
+    }
